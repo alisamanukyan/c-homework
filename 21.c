3b9c49835b81a9746bdf20e6191f721ff5e1ebac
alisamanukyan <110375886+alisamanukyan@users.noreply.github.com>
2024-11-25 20:39:01 +0400

    Add files via upload

--- 21.c
+++ 21.c
@@ -1,16 +1,30 @@
+// Write a program in C to print all the alphabets using a pointer.
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
-    
-    char *ptr = alphabets;
+    char *alph;  
+    char *ptr;   
 
-    printf("All alphabets:\n");
-    while (*ptr != '\0') {  
-        printf("%c ", *ptr); 
-        ptr++;               
+    alph = (char *)malloc(26 * sizeof(char));
+    if (alph == NULL) {  
+        printf("Memory allocation failed.\n");
+        return 1;
     }
-    
+
+    ptr = alph;  
+
+    for (int x = 0; x < 26; x++) {
+        *(ptr + x) = 'A' + x;  
+    }
+
+    printf("The Alphabets are:\n");
+    for (int x = 0; x < 26; x++) {
+        printf("%c ", *(ptr + x));
+    }
     printf("\n");
+
+    free(alph);
+
     return 0;
 }
